a1df268561825044838aa06012232474739c2f95
Pranav Sutar <pranavsutar4747@gmail.com>
2025-09-18 14:28:10 +0530

    Merge successfully & Added Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom"
 import { DoctorDashboard } from "./doctor/DoctorDashboard"
 import { HospitalDashboard } from "./hospital/HospitalDashboard"
 
 
 function App() {
-
-
   return (
-    <DoctorDashboard/>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Navigate to="/hospital" replace />} />
+        <Route path="/hospital" element={<HospitalDashboard/>}/>
+        <Route path="/doctor" element={<DoctorDashboard/>}/>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
